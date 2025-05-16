62c16c2987e8a938e39d510a5030ecbf5801d5ee
Jérôme Belpaire <jerome.belpaire@gmail.com>
2025-05-16 09:10:05 +0200

    refactor: minor change

--- src/components/RecipeRatingInput.tsx
+++ src/components/RecipeRatingInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { fetchFromBackend } from "./fetchFromBackend";
 import { useAuth } from "./AuthContext";
 
